cd404bb86684b08b8f719fd6291b072419aea84a
Solomon Amunde <71704044+Smith-gift@users.noreply.github.com>
2026-09-20 19:19:28 +0100

    Create Cngn-monthlyVolume-Eth&Bsc.sql

---
+++ dashboards/cngn-onchain-activity-tracker/Queries/Cngn-monthlyVolume-Eth&Bsc.sql
@@ -0,0 +1,20 @@
+-- ===========================
+-- AUTHOR: Solomon Gift Amunde
+-- AUTHOR BIO: Onchain, blockchain and crypto data analyst
+-- PROJECT: Cngn Adoption and Growth Analysis
+-- ===========================
+SELECT
+  blockchain,
+  date_trunc('month', block_time) AS month,
+  SUM(amount) AS volume,
+  SUM(amount_usd) AS volume_usd,
+  COUNT(*) AS transfer_count
+FROM tokens.transfers
+WHERE contract_address IN (
+    0xa8AEA66B361a8d53e8865c62D142167Af28Af058,
+    0x17CDB2a01e7a34CbB3DD4b83260B05d0274C8dab
+  )
+GROUP BY 1, 2
+ORDER BY 2, 1
+
+-- This query fetches the monthly transactions on Binance smart chain and Ethereum
