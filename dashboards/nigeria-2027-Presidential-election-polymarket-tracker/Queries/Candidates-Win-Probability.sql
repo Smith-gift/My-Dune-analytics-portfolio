0118943958494046f34dd19e94ecbf6f5bd394a0
Solomon Amunde <71704044+Smith-gift@users.noreply.github.com>
2026-09-20 09:49:11 +0100

    Create Candidates-Win-Probability.sql

---
+++ dashboards/nigeria-2027-Presidential-election-polymarket-tracker/Queries/Candidates-Win-Probability.sql
@@ -0,0 +1,20 @@
+-- ===========================
+-- AUTHOR: Solomon Gift Amunde
+-- AUTHOR BIO: Onchain, blockchain and crypto data analyst
+-- PROJECT: Nigerians involvement with presidential election prediction market 
+-- ===========================
+
+SELECT
+  m.question AS candidate,
+  p.latest_price * 100 AS implied_probability_pct
+FROM polymarket_polygon.market_prices_latest p
+JOIN polymarket_polygon.market_details m
+  ON CAST(p.condition_id AS VARCHAR) = CAST(m.condition_id AS VARCHAR)
+  AND CAST(p.token_id AS VARCHAR) = CAST(m.token_id AS VARCHAR)
+WHERE m.event_market_name = 'Nigerian Presidential Election Winner'
+  AND NOT regexp_like(m.question, 'Candidate [A-Z] win')
+  AND m.question NOT LIKE '%Other%'
+  AND m.token_outcome = 'Yes'
+ORDER BY implied_probability_pct DESC
+
+-- this query shows each candidates win probability
